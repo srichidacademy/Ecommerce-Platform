1da1adfc19baa9133b4f9eee74f726b129665341
abhishek@srichidtechnologies.com <abhishek@srichidtechnologies.com>
2020-07-01 09:17:41 +0530

    New projects structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCategoryComponent } from './components/product-category/product-category.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCategoryComponent,
+    ProductListComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
